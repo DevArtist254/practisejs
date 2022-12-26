82b7242618c7a1c7a677cb07394de88bbd2c68ac
devartist <kevinwasonga415@outlook.com>
2022-12-26 10:57:22 +0300

    end of practise

--- object.js
+++ object.js
@@ -4,3 +4,41 @@ const resturants = {
 }
 
 const {name = [], location} = resturants
+
+const xyz = {
+ x: {
+  a: 1,
+  b: 2,
+ },
+}
+
+const {
+ x: {a, b},
+} = xyz
+
+console.log(a)
+
+const arr1 = [1, 2, 3, 4]
+const arr2 = [5, 6, 7, 8]
+
+console.log([...arr1, ...arr2])
+
+/**
+ * Works with all iterables
+ * like arrays, strings, maps & sets
+ *
+ * This is a form of data struture
+ *
+ */
+
+const [s, c, ...others] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+console.log(others)
+
+//fn
+
+const add = function (...numbers) {
+ console.log(numbers)
+}
+
+add(1, 2, 3, 4, 5, 5)
